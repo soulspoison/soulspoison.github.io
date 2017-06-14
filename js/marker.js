2bc075f055e48d8f35ad671f7b620e5bebd29e44
zhangpengfei <zzppff1p@gmail.com>
2017-06-14 15:40:36 +0800

    项目初始化

---
+++ js/marker.js
@@ -0,0 +1,16 @@
+var provinces = [{
+	"name": "福瑞特",
+	"center": "114.507765,36.586708",
+	"type": 0,
+	"subDistricts": []
+}, {
+	"name": "丛台公园",
+	"center": "114.489999,36.614202",
+	"type": 1,
+	"subDistricts": []
+}, {
+	"name": "维度",
+	"center": "114.487287,36.610182",
+	"type": 2,
+	"subDistricts": []
+}];
