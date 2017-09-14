0f4d77dba57b8b04900388daa16e280ce48f29e8
soulpoison <Zz_Arrogance@126.com>
2017-09-14 17:17:57 +0800

    项目结构调整

---
+++ 3vroom/js/bottom.js
@@ -0,0 +1,3 @@
+
+parent.document.all("iframe").style.height=document.body.scrollHeight; 
+parent.document.all("iframe").style.width=document.body.scrollWidth; 
--- js/bottom.js
+++
@@ -1,3 +0,0 @@
-
-parent.document.all("iframe").style.height=document.body.scrollHeight; 
-parent.document.all("iframe").style.width=document.body.scrollWidth; 
